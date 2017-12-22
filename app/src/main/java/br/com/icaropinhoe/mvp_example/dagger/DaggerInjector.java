e8e6b5d78f8d749b7eabb17f65c2a3cd0ab23ec5
Your Name <you@example.com>
2017-12-22 15:48:57 -0200

    Basic package structure

---
+++ app/src/main/java/br/com/icaropinhoe/mvp_example/dagger/DaggerInjector.java
@@ -0,0 +1,8 @@
+package br.com.icaropinhoe.mvp_example.dagger;
+
+/**
+ * Created by icaro on 22/12/2017.
+ */
+
+public class DaggerInjector {
+}
--- app/src/main/java/br/com/icaropinhoe/mvp_example/ui/main/MainActivity.java
+++ app/src/main/java/br/com/icaropinhoe/mvp_example/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.icaropinhoe.mvp_example;
+package br.com.icaropinhoe.mvp_example.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.icaropinhoe.mvp_example.R;
 
 public class MainActivity extends AppCompatActivity {
 
